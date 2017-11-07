c9d6ab58270932a2fc9692f44c5e032101bfca80
zerofate <951839374@qq.com>
2017-11-07 20:18:26 +0800

    update

---
+++ justfortest/src/main/java/com/zerofate/template/justfortest/purchase/Goods.java
@@ -0,0 +1,73 @@
+package com.zerofate.template.justfortest.purchase;
+
+/**
+ * @author Timon
+ * @date 2017/11/1.
+ */
+
+public class Goods {
+    private String name;
+    private float price;
+    private int count;
+
+    public Goods(String name, float price, int count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+
+    public void plusCount(int increase) {
+        count = count + increase;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        return  31 * result + name.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Goods)) {
+            return false;
+        }
+        final Goods other = (Goods) obj;
+        if (name.equals(other.name)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "name: " + name + " & price: " + price + " & count: " + count;
+    }
+}
---
+++ justfortest/src/main/java/com/zerofate/template/justfortest/purchase/MeaninglessActivity.java
@@ -0,0 +1,18 @@
+package com.zerofate.template.justfortest.purchase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zerofate.template.justfortest.R;
+
+/**
+ * 没有任何意义的Activity，可能以后会加入很多乱七八糟的东西
+ */
+public class MeaninglessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meaningless);
+    }
+}
