001e0630ce8e8fe57bab09daffce37984a3615ac
zerofate <951839374@qq.com>
2017-11-15 20:33:36 +0800

    RxJava

--- app/src/main/java/com/zerofate/template/purejava/RxJavaTest.java
+++ app/src/main/java/com/zerofate/template/purejava/RxJavaTest.java
@@ -1,9 +1,22 @@
 package com.zerofate.template.purejava;
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.Observable;
 
 public class RxJavaTest {
     public static void main(String[] args) {
+        // 间隔打印
+        Observable<Long> secondIntervals = Observable.interval(1, TimeUnit.SECONDS);
+        secondIntervals.subscribe(s -> System.out.println(s));
+        try {
+            Thread.sleep(5000);// 确保主进程持续到发送完成
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         Observable<String> strings = Observable.just("Alpha", "Beta", "Gamma", "Delta", "Epsilon");
+        // 依次打印每个字符串的长度
+        strings.map(s -> s.length()).subscribe(length -> System.out.println(length));
+        // 依次打印每个字符串
         strings.subscribe(s -> System.out.println(s));
     }
 }
--- zerosdk/src/main/java/com/zerofate/androidsdk/util/Utils.java
+++ zerosdk/src/main/java/com/zerofate/androidsdk/util/Utils.java
@@ -1,8 +1,13 @@
 package com.zerofate.androidsdk.util;
 
+import android.app.ActivityManager;
+import android.content.Context;
 import android.os.Build;
+import android.os.Process;
 
 import com.zerofate.androidsdk.BuildConfig;
+
+import java.util.List;
 
 /**
  * Created by zero on 2017/8/24.
@@ -10,9 +15,10 @@ import com.zerofate.androidsdk.BuildConfig;
 
 public class Utils {
 
-    public static boolean isDebug(){
+    public static boolean isDebug() {
         return BuildConfig.DEBUG;
     }
+
     public static boolean hasFroyo() {
         // Can use static final constants like FROYO, declared in later versions
         // of the OS since they are inlined at compile time. This is guaranteed behavior.
@@ -49,7 +55,23 @@ public class Utils {
     /**
      * 26
      */
-    public static boolean hasO(){
+    public static boolean hasO() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
+    }
+
+    public static boolean isMainProcess(Context context) {
+        return context.getPackageName().equals(getProcessName(context));
+    }
+
+    public static String getProcessName(Context context) {
+        int myPid = Process.myPid();
+        ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> infos = manager.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo info : infos) {
+            if (info.pid == myPid) {
+                return info.processName;
+            }
+        }
+        return null;
     }
 }
