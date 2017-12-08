520fd0df98a76c15850d15da5b5b880758ad1704
dozeboy <hunterxwux@gmail.com>
2017-12-08 18:02:28 +0800

    update

---
+++ justfortest/src/main/java/com/zerofate/template/justfortest/Bean.java
@@ -0,0 +1,47 @@
+package com.zerofate.template.justfortest;
+
+/**
+ * @author Timon
+ * @date 2017/12/8
+ */
+
+public class Bean {
+
+    /**
+     * name : 张三
+     * age : 26
+     */
+
+    private String name;
+    private int age;
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Bean(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- justfortest/src/main/java/com/zerofate/template/justfortest/MeaninglessActivity.java
+++ justfortest/src/main/java/com/zerofate/template/justfortest/MeaninglessActivity.java
@@ -1,8 +1,18 @@
 package com.zerofate.template.justfortest;
 
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.view.ViewTreeObserver;
+
+import com.google.gson.Gson;
+import com.zerofate.androidsdk.util.ToastX;
+
 import butterknife.OnClick;
 
 /**
@@ -19,7 +29,22 @@ public class MeaninglessActivity extends HelloActivity {
 
     @OnClick(R.id.btn_hello)
     public void onHello() {
-
+        try {
+            String json = "{\n"
+                    + "    \"name\": \"张三\",\n"
+                    + "    \"agea\": \"11\",\n"
+                    + "    \"some\": 1\n"
+                    + "}";
+            Bean bean = new Gson().fromJson(json,Bean.class);
+            ToastX.showLong(this, bean.toString());
+        } catch (Exception e) {
+            ToastX.showLong(this, e.toString());
+        }
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 }
