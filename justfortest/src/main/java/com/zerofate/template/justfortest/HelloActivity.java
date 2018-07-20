5d26c5f4b573670e66ad1c690446de1fcaecbf8c
dozeboy <hunterxwux@gmail.com>
2018-07-20 18:42:02 +0800

    update

--- justfortest/src/main/java/com/zerofate/template/justfortest/HelloActivity.java
+++ justfortest/src/main/java/com/zerofate/template/justfortest/HelloActivity.java
@@ -1,17 +1,28 @@
 package com.zerofate.template.justfortest;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
 import android.text.Spanned;
+import android.text.format.Formatter;
 import android.text.style.AbsoluteSizeSpan;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.zerofate.androidsdk.util.ToastX;
 import com.zerofate.template.justfortest.databinding.ActivityHelloBinding;
 import com.zerofate.template.justfortest.lifecycleArch.UserModel;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 public class HelloActivity extends AppCompatActivity {
 
@@ -27,7 +38,54 @@ public class HelloActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_hello);
+        binding.button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastX.showLong(HelloActivity.this,getLocalIpAddress());
+            }
+        });
+
+        binding.button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ToastX.showLong(HelloActivity.this,getLocalHostIp());
+            }
+        });
+    }
+
+
+    public String getLocalIpAddress() {
+        WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        return ip;
+    }
+
+    public String getLocalHostIp() {
+        String ipaddress = "";
+        try {
+            Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces();
+            // 遍历所用的网络接口
+            while (en.hasMoreElements()) {
+                NetworkInterface nif = en.nextElement();// 得到每一个网络接口绑定的所有ip
+                Enumeration<InetAddress> inet = nif.getInetAddresses();
+                // 遍历每一个接口绑定的所有ip
+                while (inet.hasMoreElements()) {
+                    InetAddress ip = inet.nextElement();
+//                    if (!ip.isLoopbackAddress() && InetAddressUtils.isIPv4Address(ip.getHostAddress())) {
+//                        return ipaddress = ip.getHostAddress();
+//                    }
+                    if (!ip.isLoopbackAddress() && ip instanceof Inet4Address) {
+                        return ipaddress = ip.getHostAddress();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            Log.e("feige", "获取本地ip地址失败");
+            e.printStackTrace();
+        }
+        return ipaddress;
+
     }
 
 
--- zerosdk/src/main/java/com/zerofate/androidsdk/base/BaseSingleFragmentActivity.java
+++ zerosdk/src/main/java/com/zerofate/androidsdk/base/BaseSingleFragmentActivity.java
@@ -1,10 +1,15 @@
 package com.zerofate.androidsdk.base;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.zerofate.androidsdk.R;
 
@@ -14,15 +19,24 @@ import com.zerofate.androidsdk.R;
  */
 public abstract class BaseSingleFragmentActivity extends AppCompatActivity {
 
-    private static Activity sActivity;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        sActivity = this;
-        setContentView(R.layout.base_activity_single_fragment);
-        getSupportFragmentManager().beginTransaction().replace(R.id.frame_fragment_container,
-                getFragment()).commit();
+        FrameLayout frameLayout = new FrameLayout(this);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        frameLayout.setLayoutParams(params);
+        frameLayout.setId(View.generateViewId());
+        setContentView(frameLayout);
+        getSupportFragmentManager().beginTransaction().add(frameLayout.getId(),
+                getFragment(getIntent())).commit();
     }
 
-    public abstract Fragment getFragment();
+    /**
+     * 提供Fragment
+     *
+     * @param startIntent 从启动 activity 的 intent 中获取参数
+     * @return fragment
+     */
+    public abstract Fragment getFragment(Intent startIntent);
 }
