2511019f56ffab9629a41eb102d8578ec3913d15
dozeboy <hunterxwux@gmail.com>
2018-07-23 17:47:31 +0800

    update:     1. 修改 gradle add:     1. 新增 Application.kt

---
+++ core/src/main/java/com/yeahka/android/core/utli/Application.kt
@@ -0,0 +1,13 @@
+package com.yeahka.android.core.utli
+
+import android.app.Application
+
+/**
+ * @author timon
+ * @date 2018/7/23
+ */
+
+fun Application.getVersionCode() = packageManager.getPackageInfo(packageName, 0).versionCode
+
+fun Application.getVersionName() = packageManager.getPackageInfo(packageName, 0).versionName
+
