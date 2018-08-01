d514d33e5e8d85f55804ba0e358fe979c80ffefb
dozeboy <hunterxwux@gmail.com>
2018-08-01 14:32:10 +0800

    update:     1. 删除 MeaninglessActivity 中的无用 import

--- justfortest/src/main/java/com/zerofate/template/justfortest/MeaninglessActivity.kt
+++ justfortest/src/main/java/com/zerofate/template/justfortest/MeaninglessActivity.kt
@@ -1,24 +1,7 @@
 package com.zerofate.template.justfortest
 
-import android.Manifest
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
-import android.content.Context
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.support.v4.app.ActivityCompat
-import android.support.v4.content.ContextCompat
-import android.support.v4.content.PermissionChecker
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.ViewTreeObserver
-
-import com.google.gson.Gson
-import com.zerofate.androidsdk.util.ToastX
-
-import butterknife.OnClick
 
 /**
  * 没有任何意义的Activity，可能以后会加入很多乱七八糟的东西
