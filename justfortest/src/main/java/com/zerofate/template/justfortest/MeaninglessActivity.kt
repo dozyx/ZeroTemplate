e9ddb17e1c82f3a669bfc4907f4af36e7cd6aa03
dozeboy <hunterxwux@gmail.com>
2018-08-15 19:08:37 +0800

    update

--- justfortest/src/main/java/com/zerofate/template/justfortest/MeaninglessActivity.kt
+++ justfortest/src/main/java/com/zerofate/template/justfortest/MeaninglessActivity.kt
@@ -1,10 +1,15 @@
 package com.zerofate.template.justfortest
 
-import android.bluetooth.BluetoothAdapter
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import com.dozeboy.android.core.utli.log.ZLog
 
 /**
  * 没有任何意义的Activity，可能以后会加入很多乱七八糟的东西
@@ -15,8 +20,16 @@ class MeaninglessActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meaningless)
         findViewById<Button>(R.id.button).setOnClickListener {
-            val result = BluetoothAdapter.getDefaultAdapter().enable()
-            val resultDiscovery = BluetoothAdapter.getDefaultAdapter().startDiscovery()
+            var builder = NotificationCompat.Builder(this,"0").setContentTitle("哈哈哈")
+                .setContentText("ssss").setSmallIcon(R.mipmap.ic_launcher)
+            var intent = PendingIntent.getActivity(this@MeaninglessActivity,0,Intent(this@MeaninglessActivity,MeaninglessActivity.javaClass),0)
+            builder.setContentIntent(intent)
+            var manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                var channel =  NotificationChannel("0","test",NotificationManager.IMPORTANCE_HIGH)
+                manager.createNotificationChannel(channel)
+            }
+            manager.notify(111, builder.build())
         }
     }
 
