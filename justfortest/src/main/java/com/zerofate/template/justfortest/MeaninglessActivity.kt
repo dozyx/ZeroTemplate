91f86b1c4007657b757c15504102c861e4484ec5
dozeboy <hunterxwux@gmail.com>
2018-09-07 18:28:38 +0800

    push

--- justfortest/src/main/java/com/zerofate/template/justfortest/MeaninglessActivity.kt
+++ justfortest/src/main/java/com/zerofate/template/justfortest/MeaninglessActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
+import com.dozeboy.android.core.utli.log.ZLog
 
 /**
  * 没有任何意义的Activity，可能以后会加入很多乱七八糟的东西
@@ -18,6 +19,7 @@ class MeaninglessActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meaningless)
+        ZLog.d("onCreate: ")
         findViewById<Button>(R.id.button).setOnClickListener {
             var builder = NotificationCompat.Builder(this,"0").setContentTitle("哈哈哈")
                 .setContentText("ssss").setSmallIcon(R.mipmap.ic_launcher)
