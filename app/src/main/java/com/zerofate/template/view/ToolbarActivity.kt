2c6069c47bf1c515404cdd7497afb51d81831609
dozeboy <951839374@qq.com>
2018-11-04 22:59:37 +0800

    update

--- app/src/main/java/com/zerofate/template/view/ToolbarActivity.kt
+++ app/src/main/java/com/zerofate/template/view/ToolbarActivity.kt
@@ -1,13 +1,29 @@
 package com.zerofate.template.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NavUtils
 import com.zerofate.template.R
+import kotlinx.android.synthetic.main.activity_toolbar.*
 
 class ToolbarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        supportActionBar?.setHomeAsUpIndicator(android.R.drawable.arrow_up_float)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.test, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
