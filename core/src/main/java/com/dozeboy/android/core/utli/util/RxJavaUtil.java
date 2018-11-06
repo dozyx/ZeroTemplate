da08c48edec86bde88a9eb10cea2d7b873322318
timon <timon@yeahka.com>
2018-11-06 20:35:32 +0800

    update

---
+++ core/src/main/java/com/dozeboy/android/core/utli/util/RxJavaUtil.java
@@ -0,0 +1,21 @@
+package com.dozeboy.android.core.utli.util;
+
+
+import io.reactivex.SingleTransformer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * @author timon
+ * @date 2018/11/6
+ */
+public class RxJavaUtil {
+    private static final SingleTransformer schedulersTransformer = upstream -> upstream.subscribeOn(
+            Schedulers.io()).observeOn(
+            AndroidSchedulers.mainThread());
+
+    @SuppressWarnings("unchecked")
+    public static <T> SingleTransformer<T, T> applySchedulers() {
+        return (SingleTransformer<T, T>) schedulersTransformer;
+    }
+}
