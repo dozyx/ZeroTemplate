61528fa66b48d5fbf4b9770ad27b3ed1a7d53f7d
dozeboy <hunterxwux@gmail.com>
2019-01-30 19:10:57 +0800

    update

---
+++ shell-kt/src/main/java/com/dozeboy/android/template/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.dozeboy.android.template
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.dozeboy.android.template.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupDrawer()
+    }
+
+    private fun setupDrawer() {
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.drawer_open, R.string.drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+}
