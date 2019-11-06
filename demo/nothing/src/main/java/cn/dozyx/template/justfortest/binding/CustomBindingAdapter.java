ed2fd267413975ac1a58c4ff3e76b11d853cef5a
dozeboy <hunterxwux@gmail.com>
2019-11-06 18:30:35 +0800

    update

---
+++ demo/nothing/src/main/java/cn/dozyx/template/justfortest/binding/CustomBindingAdapter.java
@@ -0,0 +1,20 @@
+package cn.dozyx.template.justfortest.binding;
+
+import android.view.View;
+
+import androidx.databinding.BindingAdapter;
+
+/**
+ * Create by timon on 2019/11/6
+ **/
+public class CustomBindingAdapter {
+    @BindingAdapter("android:paddingLeft")
+    public static void setPaddingLeft(View view, int oldPadding, int newPadding) {
+        if (oldPadding != newPadding) {
+            view.setPadding(newPadding,
+                    view.getPaddingTop(),
+                    view.getPaddingRight(),
+                    view.getPaddingBottom());
+        }
+    }
+}
