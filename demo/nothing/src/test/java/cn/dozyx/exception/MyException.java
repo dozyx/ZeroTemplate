92565db6bb0c075b4ea0544aa15f1151d0900409
dozyx <951839374@qq.com>
2021-06-09 09:00:30 +0800

    update

---
+++ demo/nothing/src/test/java/cn/dozyx/exception/MyException.java
@@ -0,0 +1,11 @@
+package cn.dozyx.exception;
+
+/**
+ * @author dozyx
+ * @date 5/18/21
+ */
+public class MyException extends Exception {
+    public MyException(Throwable cause) {
+        super(cause);
+    }
+}
