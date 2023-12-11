a4e19d8ed3fa0493f795fe2fbd0610abf2458d47
Tian <hunterxwux@gmail.com>
2023-12-11 14:45:51 +0800

    Update coroutine test

--- demo/app/src/test/java/cn/dozyx/template/CoroutinesTest.kt
+++ demo/app/src/test/java/cn/dozyx/template/CoroutinesTest.kt
@@ -3,8 +3,11 @@ package cn.dozyx.template
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
+import kotlin.concurrent.thread
 
 class CoroutinesTest {
     @Test
@@ -16,5 +19,17 @@ class CoroutinesTest {
             println("launch Dispatchers.Default ${Thread.currentThread().name}")
         }
         Thread.sleep(200)
+    }
+
+    // 代码中一共启动了两个协程
+    @Test
+    fun test1() = runBlocking {
+        println(Thread.currentThread().name)
+
+        launch {
+            println(Thread.currentThread().name)
+            delay(100L)
+        }
+        Thread.sleep(1000L)
     }
 }
