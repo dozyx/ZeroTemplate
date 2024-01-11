f5bf470124da115bf7f7efe00f2a49d9f55952ed
Tian <hunterxwux@gmail.com>
2024-01-11 11:32:19 +0800

    add static broadcast

---
+++ demo/app/src/main/java/cn/dozyx/template/message/StaticBroadcastReceiver.kt
@@ -0,0 +1,19 @@
+package cn.dozyx.template.message
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import timber.log.Timber
+
+class StaticBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        Timber.d(
+            "StaticBroadcastReceiver.onReceive: {${intent?.action} {${intent?.dataString}} replacing: {${
+                intent?.getBooleanExtra(
+                    Intent.EXTRA_REPLACING,
+                    false
+                )
+            }}"
+        )
+    }
+}
