6e18cbeec04e16f7d6db7b86d4c49e97b394e5b0
Tian <hunterxwux@gmail.com>
2024-01-26 10:56:49 +0800

    test EditText

---
+++ demo/app/src/main/java/cn/dozyx/template/view/edittext/EditTextTest.kt
@@ -0,0 +1,8 @@
+package cn.dozyx.template.view.edittext
+
+import cn.dozyx.core.base.BaseActivity
+import cn.dozyx.template.R
+
+class EditTextTest : BaseActivity() {
+    override fun getLayoutId()= R.layout.edittext_test
+}
